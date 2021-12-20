c8926a31f895e6ec2ec6dc4a7923452adf822a61
Huy Vo <vohuy819@gmail.com>
2021-12-20 22:32:34 +1030

    change files location

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var char =""
 var password ="";
 var length
 
-// Write password to the #password input
 function generatePassword() {
   var length
   
@@ -17,10 +16,12 @@ function generatePassword() {
   while (i == 0) { 
    //Ask Users about Password length 
   length = window.prompt ("Please choose your password's length (>8, <128 letters): ");
+
    // If user pressed Cancel, immediately end function
   if (!length) {
     return;
   };
+
   // If password length is not satisfy condition, ask users to try again
   if ((length<=8 || length>=128 || isNaN(length))) {
       window.alert ("wrong condition, please try again!");
@@ -30,9 +31,9 @@ function generatePassword() {
   
   //Password's criteria
   function add(input,crit) {
-  if (input) {char=char+crit};
-  return;
-  };
+   if (input) {char=char+crit};
+    return;
+   };
   var alpha = window.confirm("Do you want your password contains lowercase letters?");
   add(alpha,alphabet);
   var upper = window.confirm("Do you want your passwords contains uppercase letters?")
@@ -42,6 +43,7 @@ function generatePassword() {
   var chac = window.confirm("Do you want your passwords contains speacial characters?")
   add(chac,characters);
 
+  //  Generate password
   for (var i = 0; i <= length; i++) {
     var randomNumber = Math.floor(Math.random() * char.length);
     password += char.substring(randomNumber, randomNumber +1);
