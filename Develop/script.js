e97f4c5b5cf678032c2d40cad7e0ba6ca0d6ea49
Huy Vo <vohuy819@gmail.com>
2021-12-20 17:46:31 +1030

    refactor 'User inputs' code

--- Develop/script.js
+++ Develop/script.js
@@ -6,23 +6,27 @@ var characters ="!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 var char =""
 var password ="";
 var length
+
 // Write password to the #password input
 function generatePassword() {
   var length
   
   window.alert ("Choose your password criteria");
-  //Password criteria  
-  function condition() {
-    length = window.prompt ("Please choose your password's length (>8, <128 letters): ");
-    if (!length) {
-      return;}
-    else if ((length<=8 || length>=128 || isNaN(length))) {
+  //Ask Users about Password criteria  
+  var i= 0;
+  while (i == 0) { 
+  length = window.prompt ("Please choose your password's length (>8, <128 letters): ");
+   // If user pressed Cancel, immediately end function
+  if (!length) {
+    return;
+  };
+  // If password length is not satisfy condition, ask users to try again
+  if ((length<=8 || length>=128 || isNaN(length))) {
       window.alert ("wrong condition, please try again!");
-      condition()
-      }
-    }
+      } 
+    else {i=i+1};
+  }
   
-  condition()
 
   var alpha = window.confirm("Do you want your password contains aphabet letters?");
   if (alpha) {char=char + alphabet};
